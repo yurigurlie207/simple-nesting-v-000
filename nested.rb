36cd6b313eca295e6c742a27365c7261700498b0
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 18:23:30 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
 		 programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
-		 
+
 end
 
 def changing_alan
