14feb22fd1db5329d3955827a1bc75187f5d92f3
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 17:57:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -17,13 +17,12 @@ def hopper
      }
 
 
-		 programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
-	programmer_hash =
+	
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -38,12 +37,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
-		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -58,8 +55,6 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-
-		programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -69,7 +64,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -84,14 +79,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-# programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP","C"] }
-# programmer_hash[:yukihiro_matsumoto]
 
 end
 
 def changing_alan
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -106,14 +98,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-
-
+     
+     
 end
 
 def adding_to_dennis
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
