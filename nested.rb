24a068af01afb99cbe4941a236282a32f5e15420
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 18:51:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
      }
 
 		 programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
-
+		 programmer_hash
 end
 
 def changing_alan
@@ -108,7 +108,7 @@ def changing_alan
      alans_new_info = "GUI"
 
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 programmer_hash
 end
 
 def adding_to_dennis
